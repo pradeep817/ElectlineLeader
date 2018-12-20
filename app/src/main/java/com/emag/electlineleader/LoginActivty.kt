4e61006a25ac2281c63fe2c28e757b1ab0a7c9d8
pradeep pandey <an.pradeepp@gmail.com>
2018-12-20 17:47:21 +0530

    second commit

---
+++ app/src/main/java/com/emag/electlineleader/LoginActivty.kt
@@ -0,0 +1,14 @@
+package com.emag.electlineleader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_activty)
+
+
+    }
+}
