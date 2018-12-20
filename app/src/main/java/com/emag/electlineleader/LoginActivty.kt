35fc215c791cd8423e6ed50df4bba23fa1b15b38
pradeep pandey <an.pradeepp@gmail.com>
2018-12-20 22:23:57 +0530

    third commit

--- app/src/main/java/com/emag/electlineleader/LoginActivty.kt
+++ app/src/main/java/com/emag/electlineleader/LoginActivty.kt
@@ -1,7 +1,9 @@
 package com.emag.electlineleader
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login_activty.*
 
 class LoginActivty : AppCompatActivity() {
 
@@ -10,5 +12,6 @@ class LoginActivty : AppCompatActivity() {
         setContentView(R.layout.activity_login_activty)
 
 
+        create_account.setOnClickListener { startActivity(Intent(this, RegisterActivity::class.java)) }
     }
 }
--- app/src/main/java/com/emag/electlineleader/RegisterActivity.kt
+++ app/src/main/java/com/emag/electlineleader/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.emag.electlineleader
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -9,6 +11,8 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        overridePendingTransition(R.anim.left_in,R.anim.left_out)
 
+        sign_in.setOnClickListener { startActivity(Intent(this,LoginActivty::class.java)) }
     }
 }
