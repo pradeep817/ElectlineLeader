5adadc0a4e58862ebb030df1a9ae6334f7744cb7
pradeep pandey <an.pradeepp@gmail.com>
2018-12-20 16:10:56 +0530

    second commit

--- app/src/main/java/com/emag/electlineleader/LeaderProfile.kt
+++ app/src/main/java/com/emag/electlineleader/LeaderProfile.kt
@@ -3,14 +3,10 @@ package com.emag.electlineleader
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LeaderProfile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // i am working here
-
-
+        setContentView(R.layout.activity_leader_profile)
     }
 }
